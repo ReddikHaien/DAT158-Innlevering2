80d8eae77362c5ae52715eaad8a978c6f7741e53
ReddikHaien <584969@stud.hvl.no>
2022-11-18 21:25:04 +0100

    added a small snippet of comment

--- shared.py
+++ shared.py
@@ -1,3 +1,9 @@
 import os
+
+#fastai expects every custom method to be in the same path when exporting and loading models
+#this means that we need to have an shared file for both the deployment and the notebook
+#An better solution to this would be to export the model in a more shared format instead of an fastai object
+#Like the ONNX runtime format, wich wouldn't need the entire fastai api and pytorch to run
+
 def label_func(f): 
     return os.path.basename(os.path.dirname(f))
